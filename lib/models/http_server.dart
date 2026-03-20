ec1856d9a0df00161d6382cb861af250d00d96ad
kozvits <kozvits@gmail.com>
2026-03-20 14:12:44 +0300

    Add files via upload

--- lib/models/http_server.dart
+++ lib/models/http_server.dart
@@ -1,1 +1,37 @@
+// lib/models/http_server.dart
 
+class HttpServer {
+  final String id;
+  final String name;
+  final String url;
+
+  HttpServer({
+    required this.id,
+    required this.name,
+    required this.url,
+  });
+
+  HttpServer copyWith({
+    String? id,
+    String? name,
+    String? url,
+  }) {
+    return HttpServer(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      url: url ?? this.url,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'url': url,
+      };
+
+  factory HttpServer.fromJson(Map<String, dynamic> json) => HttpServer(
+        id: json['id'] as String,
+        name: json['name'] as String,
+        url: json['url'] as String,
+      );
+}
